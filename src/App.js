71f188b64d06754d1ad12b709eade74f8e48df08
975569220@qq.com <975569220@qq.com>
2025-11-22 19:51:39 +0800

    init loading, error, ready status

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loading from "./Loading";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+
 const initialState = {
   questions: [],
   status: "loading",
@@ -24,19 +28,22 @@ function reducer(state, action) {
   }
 }
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const numQuestions = questions.length;
+
   useEffect(function () {
     fetch("http://localhost:9000/questions")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataReceived", payload: data }))
       .catch((err) => dispatch({ type: "dataFailed" }));
-  });
+  }, []);
   return (
     <div className="app">
       <Header />
       <Main className="main">
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === "loading" && <Loading />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
       </Main>
     </div>
   );
